5437ff414f533349254adc471748c62e3e6e1238
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-22 09:16:00 +0600

    did height method

--- assignment 5/src/Main.java
+++ assignment 5/src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         BST<Integer, String> tree = new BST<>();
-        tree.put(1, "One");
-        tree.put(2, "Two");
-        tree.put(3, "Three");
-        tree.put(4, "Four");
+        tree.put(1, "Audi");
+        tree.put(2, "Volvo");
+        tree.put(3, "Mercedes-Benz");
+        tree.put(4, "Porsche");
         System.out.println("The height of BST before deleting is "+tree.height());
-        System.out.println(tree.get(1));
-        tree.delete(1);
+        System.out.println(tree.get(4));
+        tree.delete(4);
 
         for (BST.NodeEntry<Integer, String> entry : tree) {
-            System.out.println("Key is " + entry.key + " and value is " + entry.value);
+            System.out.println("Key is " + entry.key + " and it is " + entry.value);
         }
         System.out.println("The height of BST after deleting is "+tree.height());
         System.out.println("The size of BST is "+tree.size());
